216a18e4b82520fafaf273dffe2f7b4eea6adb8d
reasat-0 <reasat.g8@gmail.com>
2025-05-18 21:17:58 +0600

    feat/complex routing with query handling and params handling

--- src/app.js
+++ src/app.js
@@ -4,7 +4,18 @@ const app = exp();
 
 const port = 3000;
 
+app.get("/user/:uid/:customerId/:date", (req, res) => {
+  console.log(req.params);
+
+  res.send({
+    name: "HEloo",
+    age: 123,
+  });
+});
+
 app.get("/user-profile", (req, res) => {
+  console.log(req.query);
+
   res.send({
     name: "HEloo",
     age: 123,
@@ -15,6 +26,38 @@ app.post("/user-profile", (req, res) => {
   res.send({
     status: 200,
     message: "Success",
+  });
+});
+
+// Complex route patterns....
+app.get("/ab?c", (req, res) => {
+  // will work for abc or ac... ? mark makes b optional...
+  res.send({
+    name: "HEloo",
+    age: 123,
+  });
+});
+app.get("/ab+c", (req, res) => {
+  // will work for abbbbbbbbbbbc or abc... + mark tells that you can write any amount of b keeping a and c on both side...
+  res.send({
+    name: "HEloo",
+    age: 123,
+  });
+});
+
+app.get("/ab*c", (req, res) => {
+  // will work for ab--anything-c or ... * mark tells that you can write anything after b keeping ab and c on both side...
+  res.send({
+    name: "HEloo",
+    age: 123,
+  });
+});
+
+app.get("/a(bc)?d", (req, res) => {
+  // will work for ad. Cause b and c has become optional in group
+  res.send({
+    name: "HEloo",
+    age: 123,
   });
 });
 
