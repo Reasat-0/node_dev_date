b3085afae741bc3df3f1f6e1d1d9fe113f626373
reasat-0 <reasat.g8@gmail.com>
2025-05-18 21:04:28 +0600

    feat/simple routes and http methods

--- src/app.js
+++ src/app.js
@@ -4,9 +4,21 @@ const app = exp();
 
 const port = 3000;
 
-app.use((req, res) => {
-  res.send("Hello from the server " + port);
+app.get("/user-profile", (req, res) => {
+  res.send({
+    name: "HEloo",
+    age: 123,
+  });
 });
+
+app.post("/user-profile", (req, res) => {
+  res.send({
+    status: 200,
+    message: "Success",
+  });
+});
+
+// Routes...
 
 app.use("/test", (req, res) => {
   res.send("Hello from the server " + port);
